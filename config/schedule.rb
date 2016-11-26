634c89587bf571cf633e6b77221aad987155da56
Tomoyuki Soeya <soeya@5smile.com>
2016-11-26 15:36:42 +0900

    change production

---
+++ config/schedule.rb
@@ -0,0 +1,10 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# bundle exec whenever --update-crontab
+# bundle exec whenever --clear-crontab
+set :output, "log/cron.log"
+
+
