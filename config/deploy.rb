06da8df02954837533fafc3bcc5efc57d33a397c
Tomoyuki Soeya <soeya@5smile.com>
2016-11-26 15:45:14 +0900

    change production

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_tag, "deploy-#{Time.now.strftime('%Y%m%d-%H%M-%S')}"
 # set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
-set :delayed_job_server_role, :delayed_job
+set :delayed_job_server_role
 after 'deploy:publishing'
 
 
