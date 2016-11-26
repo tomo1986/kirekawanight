49d0073ede88667f022b412786382037f1d6aea1
Tomoyuki Soeya <soeya@5smile.com>
2016-11-26 17:18:08 +0900

    change production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth
+
+  def basic_auth
+    authenticate_or_request_with_http_basic('Enter Password') do |u, p|
+      u == 'kirekawa_dev' && p == 'kirekawa123'
+    end
+  end
 end
