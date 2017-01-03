1bc68e80fc43b7dacd10def790bd6f4894287eff
Tomoyuki Soeya <soeya@5smile.com>
2017-01-03 18:28:46 +0700

    chage

--- app/controllers/api/front_controller.rb
+++ app/controllers/api/front_controller.rb
@@ -425,7 +425,7 @@ class Api::FrontController < ApiController
     page = params[:page].to_i.abs > 0 ? params[:page].to_i.abs : 1
     now = Time.zone.now
     if params[:sort] == 'new'
-      shops = Shop.joins("left join pickups on shops.id = pickups.subject_id and pickups.type = 'PickupType::Push' and pickups.subject_type = 'Shop' and shops.deleted_at is null").where("shops.job_type = ? or (pickups.start_at <= ? and pickups.end_at > ?)",params[:job_type], now,now).order("pickups.number_place is null asc, pickups.number_place asc")
+      shops = Shop.joins("left join pickups on shops.id = pickups.subject_id and pickups.type = 'PickupType::Push' and pickups.subject_type = 'Shop' and shops.deleted_at is null").where("(shops.job_type = ? and (pickups.start_at <= ? and pickups.end_at > ?)) or (shops.job_type = ?) ",params[:job_type], now,now, params[:job_type]).order("pickups.number_place is null asc, pickups.number_place asc")
     else
       shops = Shop.where(job_type: params[:job_type],deleted_at: nil)
     end
