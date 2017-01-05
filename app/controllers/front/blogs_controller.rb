81d52dcb4590ba6bc401a1900d7f28160f79dbd0
Tomoyuki Soeya <soeya@5smile.com>
2017-01-05 20:52:18 +0700

    change

--- app/controllers/front/blogs_controller.rb
+++ app/controllers/front/blogs_controller.rb
@@ -2,7 +2,7 @@ class Front::BlogsController < ApplicationController
   layout "front/blog"
 
   def index
-    @head_title = "ベトナム・ホーチミン/ハノイで夜遊びブログ！！ | 見つかる出会える女の子数No.1「キレカワ」1"
+    @head_title = "ベトナム・ホーチミン/ハノイで夜遊びブログ！！ | 見つかる出会える女の子数No.1「キレカワ」"
     @head_keyword = "ベトナム、ホーチミン、ハノイ、夜遊び、カラオケ、マッサージ、観光、ブログ、キレカワ、kirekawa"
     @head_description = "ベトナム、ホーチミン/ハノイで綺麗、可愛い女の子がきっと見つかる夜遊びサイトのブログ。カラオケ/マッサージ/バーの女の子を探すなら、キレカワ"
     limit = params[:limit].to_i.abs > 0 ? params[:limit].to_i.abs : 10
