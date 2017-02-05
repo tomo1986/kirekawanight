15f09507284c484afd4779461f096e0a7b25d5e0
Tomoyuki Soeya <soeya@5smile.com>
2017-02-05 13:00:34 +0700

    change

--- app/controllers/api/front_controller.rb
+++ app/controllers/api/front_controller.rb
@@ -167,15 +167,12 @@ class Api::FrontController < ApiController
   def api3
     PageViewType::UserDetail.create(subject_type: 'User',subject_id: params[:id])
     user = User.find_by(id: params[:id])
-    users = User.where( shop_id: user.shop_id)
-    users = users.where.not(id: user.id)
     is_favorited = false
     is_favorited = current_customer.favorites.exists?(receiver_type: 'User', receiver_id: params[:id]) if customer_signed_in?
 
     builder = Jbuilder.new do |json|
       json.user user.is_pickuped && user.is_pickuped == true ? user.to_jbuilder_for_admin : user.to_jbuilder
       json.profile user.ja_profile ?  user.ja_profile.to_jbuilder : nil
-      json.users user.shop ? User.to_jbuilders(users) : nil
       json.is_favorited is_favorited
     end
     render json: builder.target!
